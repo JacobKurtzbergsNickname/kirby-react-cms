1a0c4b1a067530d9511db8c71006d23a69a82f44
Kirby <korbinian.brunner@web.de>
2023-08-18 00:02:37 +0200

    Main Body

---
+++ src/components/main/body/Body.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const Body = createGlobalStyle`
+  body {
+    background-color: #333; /* Dark grey color */
+    margin: 0;
+    padding: 0;
+    font-family: Arial, sans-serif;
+  }
+`;
+
